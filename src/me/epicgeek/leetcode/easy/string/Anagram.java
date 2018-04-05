8f59b89f14cf25d6e4e433ed594e5a199f4a9b0f
unknown <epicgeek@126.com>
2018-04-05 21:44:44 +0800

    add anagram

---
+++ src/me/epicgeek/leetcode/easy/string/Anagram.java
@@ -0,0 +1,33 @@
+package me.epicgeek.leetcode.easy.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Anagram {
+	public static void main(String[] args) {
+		System.out.println(isAnagram("anagrams","nagaram"));
+	}
+    public static boolean isAnagram(String s, String t) {
+    	if(s == null || t == null) return false;
+    	if(s.length() != t.length()) return false;
+    	if(s.equals(t))  return true;
+		Map<Character,Integer> cMapS = new HashMap<>(); 
+		Map<Character,Integer> cMapT = new HashMap<>();
+		char[] sA = s.toCharArray();
+		char[] tA = t.toCharArray();
+		for (int i = 0 ; i < s.length() ; i++) {
+			putInMap(cMapS , sA[i]);
+			putInMap(cMapT , tA[i]);
+		}
+		return cMapS.equals(cMapT);
+    }
+    
+    public static void putInMap(Map<Character,Integer> cMap , char tA) {
+    	if(cMap.containsKey(tA)) {
+    		Integer i = cMap.get(tA) + 1;
+    		cMap.put(tA, i);
+    	}else {
+    		cMap.put(tA, 0);
+    	}
+    }
+}
