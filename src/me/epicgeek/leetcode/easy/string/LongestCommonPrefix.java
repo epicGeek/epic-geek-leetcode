181aff15ade78c272f0048bb7101c33d5c486dc7
Neil Pei <neilpei@synnex.com>
2018-04-06 15:30:04 +0800

    longest prefix complished

--- src/me/epicgeek/leetcode/easy/string/LongestCommonPrefix.java
+++ src/me/epicgeek/leetcode/easy/string/LongestCommonPrefix.java
@@ -3,31 +3,42 @@ package me.epicgeek.leetcode.easy.string;
 public class LongestCommonPrefix {
 	public static void main(String[] args) {
 		System.out.println(longestCommonPrefix(new String[] {
-				"aa","aa"
+				"aa","aab"
 		}));
 	}
 	// https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/40/
 	public static String longestCommonPrefix(String[] strs) {
-		String cut = "";
+		if(strs.length == 0) return "";
+		int pointer = 0;
 		String ans = "";
-		for (int i = 0; i <= strs.length; i++) {
-			int flag = 0ERROR;  //TODO PROBLEM here!!!!
-			cut = strs[0].substring(0, flag);
-			System.out.println("cut is :"+cut);
-			System.out.println("i is :"+i);
-			if (!strs[i].startsWith(cut)) {
-				System.out.println(strs[i]+"is not start with "+cut +",return "+ans);
+		int minLen = getMinLen(strs);
+		while(pointer <= minLen) {
+			String cutter = strs[0].substring(0, pointer);
+			if(allStartsWithCutter(cutter,strs)) {
+				ans = cutter;
+				pointer ++;
+			}else {
 				return ans;
 			}
-			if (i == strs.length - 1) {
-				System.out.println("Got last str.");
-				flag++;
-				System.out.println("flag add as:"+flag);
-				ans = cut;
-				System.out.println("ans updated as :"+ans);
-				i = 0;
+		}
+
+		return ans;
+	}
+	private static int getMinLen(String[] strs) {
+		int minLen = strs[0].length();
+		for (String string : strs) {
+			if(string.length()<minLen) {
+				minLen = string.length();
 			}
 		}
-		return ans;
+		return minLen;
+	}
+	private static boolean allStartsWithCutter(String cutter, String[] strs) {
+		for (String string : strs) {
+			if(!string.startsWith(cutter)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
